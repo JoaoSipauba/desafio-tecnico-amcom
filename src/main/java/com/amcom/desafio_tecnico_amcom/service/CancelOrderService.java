14ea520604b2f96659036b77ff4cc598df184832
JoaoSipauba <joaosipauba@hotmail.com>
2025-11-08 14:01:11 -0300

    feat: Implement CancelOrderService for handling order cancellations

---
+++ src/main/java/com/amcom/desafio_tecnico_amcom/service/CancelOrderService.java
@@ -0,0 +1,27 @@
+package com.amcom.desafio_tecnico_amcom.service;
+
+import com.amcom.desafio_tecnico_amcom.exception.NotFoundException;
+import com.amcom.desafio_tecnico_amcom.model.entity.Order;
+import com.amcom.desafio_tecnico_amcom.model.enumeration.OrderStatus;
+import com.amcom.desafio_tecnico_amcom.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CancelOrderService {
+
+    private final OrderRepository orderRepository;
+
+    @Transactional
+    public void execute(String orderId) {
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new NotFoundException("Order not found with id: " + orderId));
+
+        if (!OrderStatus.CANCELED.equals(order.getStatus())) {
+            order.setStatus(OrderStatus.CANCELED);
+            orderRepository.save(order);
+        }
+    }
+}
